6b80189b3f1d8da3e3c96d7822d30ca544fe6ad0
daniel.watson <daniel.watson@bioclinica.com>
2023-04-10 08:41:54 -0400

    3 method nesting

--- src/main/java/org/thshsh/util/FunctionUtils.java
+++ src/main/java/org/thshsh/util/FunctionUtils.java
@@ -5,17 +5,19 @@ import java.util.function.Function;
 public class FunctionUtils {
 	
 	public static <A,B,Z> Function<A,Z> nested(Function<A,B> fstart,Function<B, Z> fend) {
-		return (t) -> {
-			B r = fstart.apply(t);
-			return fend.apply(r);
-		};
+		return nestedDefault(fstart, fend, (Z)null);
 	}
 	
-	public static <A,B,C,Z> Function<A,Z> nested(Function<A,B> fstart,Function<B, C> f1,Function<C, Z> fend) {
+	public static <A,B,C,Z> Function<A,Z> nested(Function<A,B> fstart,Function<B, C> fmiddle,Function<C, Z> fend) {
+		Function<A,C> f0 = nestedDefault(fstart, fmiddle, (C)null);
+		return nestedDefault(f0, fend, (Z)null);
+	}
+	
+	public static <A,B,Z> Function<A,Z> nestedDefault(Function<A, B> fstart, Function<B,Z> fend, Z nullValue) {
 		return (t) -> {
-			B b = fstart.apply(t);
-			C c = f1.apply(b);
-			return fend.apply(c);
+			B r = fstart.apply(t);
+			if(r == null) return nullValue;
+			return fend.apply(r);
 		};
 	}
 
